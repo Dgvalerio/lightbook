450a5b0ffe997df0077bd3213b57d32d5f64ef9a
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2016-10-06 12:54:57 -0300

    12:54

--- js/alert/js.js
+++ js/alert/js.js
@@ -1,11 +1,13 @@
 function alerteEsp(n) {
     switch (n) {
-        case 1: page = "ext.php";       break;
-        case 2: page = "wall/01.php";   break;
-        case 3: page = "tlh/index.php";   break;
-        case 4: page = "alt/index.php";   break;
-        case 11: page = "../ext.php";   break;
-        case 12: page = "../index.php";   break;
+        case 1:
+            $('<div> ok </div>', {
+                id: 'minhaDiv',
+                class: 'minhaClasse'
+            }).appendTo('body');
+
+
+            break;
+        case 2: break;
     }
-    location.href = page;
 }
