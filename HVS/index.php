092b3d66a9f2fea2ec8651492c7c780a579dda26
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2016-10-06 10:45:21 -0300

    3

---
+++ HVS/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="../bootstrap-4.0.0-alpha.4/dist/css/bootstrap.css">
+    <script src="../js/jquery-3.1.0.min.js"> </script>
+    <script src="../js/js.js"> </script>
+    <script src="../bootstrap-4.0.0-alpha.4/dist/js/bootstrap.min.js"> </script>
+    <link rel="stylesheet" type="text/css" href="../css/01.css">
+    <title> HVS </title>
+    <script src="../js/alert/js.js"></script>
+
+</head>
+<body class="container text-md-center"> <br/>
+<header> <h1> HVS </h1> <br/></header>
+<section>
+
+<div class="btn-group-vertical btn-block" role="group">
+    <button class="labPer btn btn-primary btn-lg"> <a class="aa" href="Sketup_2015_32_e_64%20bits_+_crack.rar">Sketchup</a> </button>
+    <button class="labPer btn btn-primary btn-lg bd-top"> Firefox </button>
+    <p><?php ?></p>
+</div>
+
+</section>
+</body>
+</html>
